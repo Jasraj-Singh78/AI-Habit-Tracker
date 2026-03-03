f832389edd6d490bca954b14098f827ffd1ba5b5
Your Name <jasraj0504.becse24@chitkara.edu.in>
2026-03-03 16:38:02 +0530

    Initial backend setup with MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 
+console.log("MONGO_URI VALUE:");
+console.log(process.env.MONGO_URI);
+
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
