4604e55b78638360d7ab09c0382ce4fb8e0f8295
Your Name <jasraj0504.becse24@chitkara.edu.in>
2026-04-21 14:54:55 +0530

    modified files

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
-    res.send("IKR");
+    res.send("AI Study Habit Tracker API Running");
 });
 
 module.exports = app;
